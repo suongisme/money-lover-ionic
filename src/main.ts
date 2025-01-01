dca8d6b33cc986c4a2b113548641888959a62142
SuongNguyen <suongnguyenytm2001@gmail.com>
2025-01-01 12:47:36 +0700

    add: PWA

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,32 @@
-import { enableProdMode } from '@angular/core';
+import { enableProdMode, isDevMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import {
-  PreloadAllModules,
-  provideRouter,
-  RouteReuseStrategy,
-  withPreloading,
+    PreloadAllModules,
+    provideRouter,
+    RouteReuseStrategy,
+    withPreloading,
 } from '@angular/router';
 import {
-  IonicRouteStrategy,
-  provideIonicAngular,
+    IonicRouteStrategy,
+    provideIonicAngular,
 } from '@ionic/angular/standalone';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { environment } from './environments/environment';
+import { provideServiceWorker } from '@angular/service-worker';
 
 if (environment.production) {
-  enableProdMode();
+    enableProdMode();
 }
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
-    provideIonicAngular({mode: 'md'}),
-    provideRouter(routes, withPreloading(PreloadAllModules)),
-  ],
+    providers: [
+        { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+        provideIonicAngular({ mode: 'md' }),
+        provideRouter(routes, withPreloading(PreloadAllModules)),
+        provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000',
+        }),
+    ],
 });
